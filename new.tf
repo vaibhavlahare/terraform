7fe612930054803a8070ee80d65cf8ed0c81390c
vaibhav <vaibhavlahare3618@gmail.com>
2025-05-30 15:09:50 +0530

    added file

---
+++ new.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "name" {
+  ami = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  key_name = "new-key.pem"
+
+  tags = {
+    name = "mywebapp"
+  }
+}
