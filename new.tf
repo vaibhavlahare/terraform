9ca4c73c2f327d81e9634c3b6de4125ad756ddaf
vaibhav <vaibhavlahare3618@gmail.com>
2025-08-28 14:42:54 +0530

    resource

--- new.tf
+++ new.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "new instance" {
+resource "aws_instance" "new_instance" {
   ami = "ami-084568db4383264d4"
   instance_type = "t2.micro"
   associate_public_ip_address = true
